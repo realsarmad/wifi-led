6951983b23f689178850f514bb4d4aa27fcee082
realsarmad <sarmadiftekhar20@gmail.com>
2021-10-14 16:15:48 +0500

    created a wifi operated led light

---
+++ wifi_led_command.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+
+WiFiClient client;
+WiFiServer server(80);
+
+#define led D5
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.softAP("Sarmad MCU", "123456789");
+  Serial.println();
+  Serial.println("NodeMCU Started!");
+  Serial.println(WiFi.softAPIP());
+  server.begin();
+  pinMode(led, OUTPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  client = server.available();
+  //Gets a client that is connected to the server and has data available for reading.    
+  if (client == 1)
+  {  
+    String request =  client.readStringUntil('\n');
+    Serial.println(request);
+    request.trim();
+    if(request == "GET /ledon HTTP/1.1")
+    {
+      digitalWrite(led, HIGH);
+      Serial.println("LED is ON");
+    }
+    if(request == "GET /ledoff HTTP/1.1")
+    {
+      digitalWrite(led, LOW);
+      Serial.println("LED is OFF");
+    }
+  }
+}
